6f693a6811955c9bf072094c9a2285f82ad9074e
Manikandan <132765355+mr-manikandan@users.noreply.github.com>
2023-11-06 22:34:34 +0530

    Add files via upload

---
+++ Task 2/index.js
@@ -0,0 +1,9 @@
+function validate(){
+    var name=document.getElementById("username").value;
+    var email=document.getElementById("email").value;
+    var password=document.getElementById("p1").value;
+
+    console.log(name,email,password)
+
+
+}
